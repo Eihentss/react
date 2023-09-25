40e87a1f9e3c841bc415a9e569d4f445f89c5eba
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-25 14:49:38 +0300

    Pirma stundu saraksta versija

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Diena from "./Diena";
+
+function App() {
+  return  (
+    <>
+      <h1>Stundu saraksts</h1>    
+      <Diena nosaukums="pirmdiena"/>
+      <Diena nosaukums="otrdiena"/>
+      <Diena nosaukums="tresdiena"/>
+      <Diena nosaukums="ceturtdiena"/>
+      <Diena nosaukums="piektdiena"/>
+
+    </>
+  )
+}
+
+export default App
