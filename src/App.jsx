1a16a5dfa338d78e79201322798944022b398c5b
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-27 15:07:17 +0300

    09.27.23

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Diena from "./Diena";
 
 function App() {
+  const primdienastundas = [
+    "Sociālās zinības un vēsture",
+    "Krievu valoda",
+    "Sistēmu programmēšana",
+    "Sistēmu programmēšana"
+];
+const otrdienasstunda = [
+    "Sociālās zinības un vēsture",
+    "Dabaszinības",
+    "Matemātika",
+    "Sports"
+];
+const tresdienasstunda = [
+    "Datorsistēmas un datortīkli",
+    "Datorsistēmas un datortīkli",
+    "Sistēmu programmēšana",
+    "Sistēmu programmēšana"
+];
   return  (
     <>
-      <h1>Stundu saraksts</h1>    
-      <Diena nosaukums="pirmdiena"/>
-      <Diena nosaukums="otrdiena"/>
-      <Diena nosaukums="tresdiena"/>
-      <Diena nosaukums="ceturtdiena"/>
-      <Diena nosaukums="piektdiena"/>
+      <h3>Stundu saraksts</h3>    
+      <Diena nosaukums="pirmdiena" stundas ={primdienastundas}/>
+      <Diena nosaukums="otrdiena" stundas ={otrdienasstunda}/>
+      <Diena nosaukums="tresdiena" stundas ={tresdienasstunda}/>
+
 
     </>
   )
--- src/Diena.jsx
+++ src/Diena.jsx
@@ -1,4 +1,20 @@
+
+import Stunda from "./Stunda";
 function Diena(props) {
-return <p>Sodien ir {props.nosaukums}</p>
+
+return (
+    <>
+    <h1>Sodien ir {props.nosaukums}</h1>
+    <ol>
+    <Stunda name={props.stundas[0]}/>
+    <Stunda name={props.stundas[1]}/>
+    <Stunda name={props.stundas[2]}/>
+    <Stunda name={props.stundas[3]}/>
+    
+    
+    </ol>
+
+    </>
+)
 }
 export default Diena
